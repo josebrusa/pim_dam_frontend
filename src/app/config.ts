0c594d4443de051dbeb2d95f6e919c8db1966da6
José Luis Brusa <josebrusa@outlook.com>
2026-06-20 17:07:48 +0200

    Refactor API URL configuration in appConfig
    - Adjusted the logic for setting apiUrl in config.ts to prioritize the VITE_API_URL environment variable, ensuring a more flexible configuration for API requests based on the environment.

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
 export const appConfig = {
-  apiUrl: import.meta.env.PROD ? '/api/v1' : (import.meta.env.VITE_API_URL ?? 'http://localhost:3000/api/v1'),
+  apiUrl: import.meta.env.VITE_API_URL ?? (import.meta.env.PROD ? '/api/v1' : 'http://localhost:3000/api/v1'),
   appName: 'Lumify PIM',
 } as const;
